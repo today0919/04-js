a52e371160ae1e2ac14408afc644c9aa97a86dab
today0919 <oha0919@gmail.com>
2024-10-18 16:13:25 +0900

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,30 @@
+// btn을 클릭했을 때
+$(".btn1")
+  .on("mouseenter", function () {
+    $(".box").stop().slideDown(1000);
+  })
+  .on("mouseleave", function () {
+    $(".box").stop().slideUp(1000);
+  });
+
+$(".btn2")
+  .mouseenter(function () {
+    $(".box2").hide();
+  })
+  .mouseleave(function () {
+    $(".box2").show();
+  });
+
+// animate
+// .btn3을 클릭했을 때
+$(".btn3").click(function () {
+  // .box3가 1초 동안 왼쪽으로 100px 이동
+  $(".box3").animate(
+    {
+      width: "+=150px",
+      left: 300,
+      top: 100,
+    },
+    1000
+  );
+});
